d052e8f2b82a971eebca463b9007a79059f09e69
이정민 <whljm1003@gmail.com>
2021-12-15 15:13:01 +0900

    5.12 Refactoring

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,78 +1,18 @@
-import { useForm } from "react-hook-form";
-import { atom, useRecoilState } from "recoil";
-
-/* 
-  function ToDoList() {
-  const [todo, setTodo] = useState("");
-  const [ToDoError, setToDoError] = useState("");
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setToDoError("");
-    setTodo(value);
-  };
-
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    if (todo.length < 10) {
-      return setToDoError("To do should be longer");
-    }
-    console.log("submit");
-  };
-
-  return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input onChange={onChange} value={todo} placeholder="Write a to do" />
-        <button>Add</button>
-        {ToDoError !== "" ? ToDoError : null}
-      </form>
-    </div>
-  );
-} */
-interface IForm {
-  toDo: string;
-}
-
-interface IToDo {
-  text: string;
-  id: number;
-  category: "TO_DO" | "DOING" | "DONE";
-}
-
-const toDoState = atom<IToDo[]>({
-  key: "toDo",
-  default: [],
-});
+import { useRecoilValue } from "recoil";
+import { toDoState } from "../atoms";
+import CreateToDo from "./CreateToDo";
+import ToDo from "./ToDo";
 
 function ToDoList() {
-  const [toDos, setToDos] = useRecoilState(toDoState);
-  const { register, handleSubmit, setValue } = useForm<IForm>();
-  const handleValid = ({ toDo }: IForm) => {
-    setToDos((oldToDos) => [
-      { text: toDo, id: Date.now(), category: "TO_DO" },
-      ...oldToDos,
-    ]);
-    setValue("toDo", "");
-  };
-
+  const toDos = useRecoilValue(toDoState);
   return (
     <div>
       <h1>To Dos</h1>
       <hr />
-      <form onSubmit={handleSubmit(handleValid)}>
-        <input
-          {...register("toDo", {
-            required: "Please write a To Do",
-          })}
-          placeholder="Write a to do"
-        />
-        <button>Add</button>
-      </form>
+      <CreateToDo />
       <ul>
         {toDos.map((toDo) => (
-          <li key={toDo.id}>{toDo.text}</li>
+          <ToDo key={toDo.id} {...toDo} />
         ))}
       </ul>
     </div>
