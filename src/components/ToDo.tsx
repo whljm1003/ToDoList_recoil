0e874229bd6b0281685ca3851c4f1ff18574e8eb
이정민 <whljm1003@gmail.com>
2021-12-15 15:22:17 +0900

    5.13 Categories

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,11 +1,32 @@
-import { IToDo } from "../atoms";
-function ToDo({ text }: IToDo) {
+import React from "react";
+import { useSetRecoilState } from "recoil";
+import { IToDo, toDoState } from "../atoms";
+
+function ToDo({ text, id, category }: IToDo) {
+  const setToDos = useSetRecoilState(toDoState);
+  const onclick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const {
+      currentTarget: { name },
+    } = event;
+  };
   return (
     <li>
       <span>{text}</span>
-      <button>Doing</button>
-      <button>To Do</button>
-      <button>Done</button>
+      {category !== "DOING" && (
+        <button name="DOING" onClick={onclick}>
+          Doing
+        </button>
+      )}
+      {category !== "TO_DO" && (
+        <button name="TO_DO" onClick={onclick}>
+          To Do
+        </button>
+      )}
+      {category !== "DONE" && (
+        <button name="DONE" onClick={onclick}>
+          Done
+        </button>
+      )}
     </li>
   );
 }
