193addb6957b782cc6d3e0b315142b28a7f9d4dd
이정민 <whljm1003@gmail.com>
2021-12-15 17:13:34 +0900

    5.18 Enums

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,14 +1,24 @@
 import { atom, selector } from "recoil";
 
+/*
+	enum은 개발자를 도와주는 녀석임
+	enum의 type은 숫자 0,1,2 임 
+	그래서 "TO_DO" = "TO_DO" 로 타입 설정 해줌
+ */
+export enum Categories {
+  "TO_DO" = "TO_DO",
+  "DOING" = "DOING",
+  "DONE" = "DONE",
+}
 export interface IToDo {
   text: string;
   id: number;
-  category: "TO_DO" | "DOING" | "DONE";
+  category: Categories;
 }
 
-export const categoryState = atom({
+export const categoryState = atom<Categories>({
   key: "category",
-  default: "TO_DO",
+  default: Categories.TO_DO,
 });
 
 export const toDoState = atom<IToDo[]>({
--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useSetRecoilState } from "recoil";
-import { IToDo, toDoState } from "../atoms";
+import { Categories, IToDo, toDoState } from "../atoms";
 
 function ToDo({ text, id, category }: IToDo) {
   const setToDos = useSetRecoilState(toDoState);
@@ -25,18 +25,18 @@ function ToDo({ text, id, category }: IToDo) {
   return (
     <li>
       <span>{text}</span>
-      {category !== "DOING" && (
-        <button name="DOING" onClick={onclick}>
+      {category !== Categories.DOING && (
+        <button name={Categories.DOING} onClick={onclick}>
           Doing
         </button>
       )}
-      {category !== "TO_DO" && (
-        <button name="TO_DO" onClick={onclick}>
+      {category !== Categories.TO_DO && (
+        <button name={Categories.TO_DO} onClick={onclick}>
           To Do
         </button>
       )}
-      {category !== "DONE" && (
-        <button name="DONE" onClick={onclick}>
+      {category !== Categories.DONE && (
+        <button name={Categories.DONE} onClick={onclick}>
           Done
         </button>
       )}
